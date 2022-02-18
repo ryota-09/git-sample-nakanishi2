44eaf475311537e8a58957fc00e40aa1bbaf23c9
ryota-09 <n.ryo950911@gmail.com>
2022-02-18 11:55:20 +0900

    carクラスのフィールド変数設定

--- src/car.ts
+++ src/car.ts
@@ -1,1 +1,11 @@
-export class Car {}
+export class Car {
+  constructor(private _speed: number) {}
+
+  public get speed(): number {
+    return this._speed;
+  }
+
+  public set speed(speed: number) {
+    this._speed = speed;
+  }
+}
